4bd14b234e564866cad37970ab6833fc753449fb
Docam-cod <nesterov.andrey-nester2017@yandex.ru>
2024-02-19 22:54:46 +0300

    Добавьте файлы проекта.

---
+++ ExampleGitHub/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleGitHub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Это приложение для GitHub.\nСейчас нажму кнопку и должны появиться ваши команды");
+            Console.ReadLine();
+
+            //Писать тут
+            
+
+
+            //Тут не писать
+
+            Console.ReadKey();
+
+        }
+    }
+}
